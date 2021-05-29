f7a6b4f9b500d9a17a249c3fb177e4205ff8119b
NikolayPolegaev <N5445@yandex.ru>
2021-05-29 19:47:54 +0300

    2. Создать контейнер на базе связанного списка [#159]

--- src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
+++ src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
@@ -3,8 +3,11 @@ package ru.job4j.collection;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleLinkedListTest {
@@ -46,5 +49,43 @@ public class SimpleLinkedListTest {
         assertThat(second.next(), Is.is(2));
         assertThat(second.hasNext(), Is.is(false));
     }
+    @Test(expected = NoSuchElementException.class)
+    public void whenNoSuchElementException() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(10);
+        Iterator<Integer> it = list.iterator();
+        it.next();
+        it.next();
+    }
 
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenConcurrentModificationException() {
+        List<Double> list = new SimpleLinkedList<>();
+        list.add(10.0);
+        list.add(20.0);
+        Iterator<Double> it = list.iterator();
+        it.next();
+        list.add(30.0);
+        it.next();
+    }
+
+    @Test
+    public void whenGetFirstAndLastElement() {
+        List<Character> list = new SimpleLinkedList<>();
+        list.add('a');
+        list.add('b');
+        list.add('c');
+        list.add('d');
+        assertThat(list.get(0), is('a'));
+        assertThat(list.get(3), is('d'));
+    }
+
+    @Test
+    public void whenAddAndGetNull() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(null);
+        list.add(1);
+        assertNull(list.get(0));
+        assertThat(list.get(1), Is.is(1));
+    }
 }

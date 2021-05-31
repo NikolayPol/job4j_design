8e2d6ed1fbb7c41472a0be9f0e1c85b25f365a69
NikolayPolegaev <N5445@yandex.ru>
2021-05-31 19:37:14 +0300

    5. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -14,6 +14,25 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
     public String toString() {
         return "User{"
                 + "name='" + name + '\''
@@ -29,6 +48,8 @@ public class User {
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
 
         Set<Map.Entry<User, Object>> set = map.entrySet();
         for (Map.Entry<User, Object> el : set) {

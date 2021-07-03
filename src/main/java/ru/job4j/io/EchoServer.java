50a8ab0432268f436ef3a8f0646b257f4b7d073b
NikolayPolegaev <N5445@yandex.ru>
2021-07-04 01:06:22 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,14 +16,23 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    String resp;
                     for (String str = in.readLine();
                          str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("/?msg=Bye")) {
+                        if (str.contains("/?msg=Exit")) {
                             server.close();
                         }
+                        if (str.contains("/?msg")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.flush();
+                            out.write("Hello, dear friend.\r\n".getBytes());
+                            out.flush();
+                            resp = str.split("=")[1];
+                            out.write(resp.getBytes(StandardCharsets.UTF_8));
+                            out.flush();
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
             }
         }

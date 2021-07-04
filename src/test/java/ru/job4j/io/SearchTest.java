fc1a50c25275615f1c94dbc236b3eb3ee379469d
NikolayPolegaev <N5445@yandex.ru>
2021-07-04 20:55:46 +0300

    Refactoring 4.07.21 20:50

--- src/test/java/ru/job4j/io/SearchTest.java
+++ src/test/java/ru/job4j/io/SearchTest.java
@@ -15,15 +15,13 @@ import static org.junit.Assert.*;
 public class SearchTest {
     @Test
     public void testSearch() throws IOException {
-        Path start = Paths.get(".");
+        Path start = Paths.get("D:\\JD\\PROJECTS\\job4j_design");
         Predicate<Path> condition = p -> p.getFileName().toString().endsWith("js");
         List<Path> res = Search.search(start, condition);
         Iterator<Path> it = res.listIterator();
         List<String> exp = List.of(
-                "D:\\JD\\PROJECTS\\job4j_design\\."
-                        + "\\target\\site\\jacoco\\jacoco-resources\\prettify.js",
-                "D:\\JD\\PROJECTS\\job4j_design\\."
-                        + "\\target\\site\\jacoco\\jacoco-resources\\sort.js");
+                "D:\\JD\\PROJECTS\\job4j_design\\data\\prettify.js",
+                "D:\\JD\\PROJECTS\\job4j_design\\data\\sort.js");
         assertThat(it.next().toString(), is(exp.get(0)));
         assertThat(it.next().toString(), is(exp.get(1)));
     }

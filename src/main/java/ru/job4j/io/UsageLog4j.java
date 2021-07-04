9f7b161331d187843c4ca60e57af680f099ed659
NikolayPolegaev <N5445@yandex.ru>
2021-07-04 15:35:00 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -4,14 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
-    //private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        boolean a = false;
+        char b = '1';
+        byte c = 2;
+        short d = 3;
+        int e = 4;
+        long f = 5;
+        float g = 6;
+        double h = 7;
+        LOG.debug("LogOut: a : {}, b : {}, c : {}, d : {}, e : {}, f : {}, g : {}, h : {}",
+                a, b, c, d, e, f, g, h);
+        LOG.debug(String.valueOf(a));
+        LOG.debug(String.valueOf(b));
+        LOG.debug(String.valueOf(c));
+        LOG.debug(String.valueOf(d));
+        LOG.debug(String.valueOf(e));
+        LOG.debug(String.valueOf(f));
+        LOG.debug(String.valueOf(g));
+        LOG.debug(String.valueOf(h));
     }
 }

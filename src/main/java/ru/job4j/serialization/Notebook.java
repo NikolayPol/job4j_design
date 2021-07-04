97750f67cc94668cf3b489b999667baebd866673
NikolayPolegaev <N5445@yandex.ru>
2021-07-04 21:46:59 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/Notebook.java
+++ src/main/java/ru/job4j/serialization/Notebook.java
@@ -3,14 +3,32 @@ package ru.job4j.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "notebook")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Notebook {
-    private final boolean workStation;
-    private final float size;
-    private final String name;
-    private final Processor cpu;
-    private final String[] characteristics;
+    @XmlAttribute
+    private boolean workStation;
+    @XmlAttribute
+    private float size;
+    @XmlAttribute
+    private String name;
+    private Processor cpu;
+    private String[] characteristics;
+
+    public Notebook() {
+    }
 
     public Notebook(boolean workStation, float size, String name, Processor cpu,
                     String[] characteristics) {
@@ -32,7 +50,7 @@ public class Notebook {
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         Notebook notebook = new Notebook(
                 false,
                 15,
@@ -40,16 +58,39 @@ public class Notebook {
                 new Processor("core-i5", 4),
                 new String[]{"black", "2kg"});
 
-        Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(notebook));
-        String notebookJson = "{"
-                + "\"workStation\":false,"
-                + "\"size\":15,"
-                + "\"name\":\"HP\","
-                + "\"cpu\": " + "{" + "\"name\":\"core-i5\", \"cores\": 4" + "},"
-                + "\"characteristics\" : " + "[\"black\",\"2kg\"]"
-                + "}";
-        Notebook notebookMod = gson.fromJson(notebookJson, Notebook.class);
-        System.out.println(notebookMod);
+//        Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(notebook));
+//        String notebookJson = "{"
+//                + "\"workStation\":false,"
+//                + "\"size\":15,"
+//                + "\"name\":\"HP\","
+//                + "\"cpu\": " + "{" + "\"name\":\"core-i5\", \"cores\": 4" + "},"
+//                + "\"characteristics\" : " + "[\"black\",\"2kg\"]"
+//                + "}";
+//        Notebook notebookMod = gson.fromJson(notebookJson, Notebook.class);
+//        System.out.println(notebookMod);
+
+        // Получаем контекст для доступа к АПИ
+        JAXBContext context = JAXBContext.newInstance(Notebook.class);
+        // Создаем сериализатор
+        Marshaller marshaller = context.createMarshaller();
+        // Указываем, что нам нужно форматирование
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        try (StringWriter writer = new StringWriter()) {
+            // Сериализуем
+            marshaller.marshal(notebook, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            try (StringReader reader = new StringReader(result)) {
+                // десериализуем
+                Notebook rsl = (Notebook) unmarshaller.unmarshal(reader);
+                System.out.println(rsl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ru/job4j/serialization/Processor.java
+++ src/main/java/ru/job4j/serialization/Processor.java
@@ -1,8 +1,17 @@
 package ru.job4j.serialization;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "cpu")
 public class Processor {
-    private final String name;
-    private final int cores;
+    @XmlAttribute
+    private String name;
+    @XmlAttribute
+    private int cores;
+
+    public Processor() {
+    }
 
     public Processor(String name, int cores) {
         this.name = name;

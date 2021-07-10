fdc449117882c01ffb4fa52c624c5815b59e5608
NikolayPolegaev <N5445@yandex.ru>
2021-07-10 20:09:48 +0300

    10.07.21 8:10

---
+++ src/main/java/ru/job4j/sql/2.2.sql
@@ -0,0 +1,36 @@
+create table students(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table courses(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table students_courses(
+    id serial primary key,
+    student_id int references students(id),
+    course_id int references courses(id)
+);
+
+insert into students(name) values ('Ivan');
+insert into students(name) values ('Kirill');
+insert into students(name) values ('Roman');
+select * from students;
+
+insert into courses(name) values ('Java SE');
+insert into courses(name) values ('Spring');
+insert into courses(name) values ('Hibernate');
+-- delete  from courses;
+-- alter sequence courses_id_seq restart with 1;
+select * from courses;
+
+insert into students_courses(student_id, course_id) values (1, 1);
+insert into students_courses(student_id, course_id) values (1, 2);
+insert into students_courses(student_id, course_id) values (1, 3);
+insert into students_courses(student_id, course_id) values (2, 1);
+insert into students_courses(student_id, course_id) values (2, 2);
+insert into students_courses(student_id, course_id) values (3, 3);
+select * from students_courses;
+

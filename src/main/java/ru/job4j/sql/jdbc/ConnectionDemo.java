47a57a8fd1d695800b16b2e9a1b8bee193ab17bd
NikolayPolegaev <N5445@yandex.ru>
2021-07-17 09:20:16 +0300

    0. JDBC [#6863 #177083]

--- src/main/java/ru/job4j/sql/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/sql/jdbc/ConnectionDemo.java
@@ -2,37 +2,27 @@ package ru.job4j.sql.jdbc;
 
 import org.postgresql.Driver;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String url = "";
-        String login = "";
-        String password = "";
-        try (BufferedReader in = new BufferedReader(new FileReader(
-                "src/main/java/ru/job4j/sql/jdbc/app.properties"))){
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                if (line.contains("url")) {
-                    url = line.split("=")[1];
-                }
-                if (line.contains("login")) {
-                    login = line.split("=")[1];
-                }
-                if (line.contains("password")) {
-                    password = line.split("=")[1];
-                }
-            }
+        Properties properties = new Properties();
+        String PATH_TO_PROPERTIES =
+                "src/main/java/ru/job4j/sql/jdbc/app.properties";
+        try {
+            properties.load(new FileInputStream(PATH_TO_PROPERTIES));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
